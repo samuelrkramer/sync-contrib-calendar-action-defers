3bb42e3789668f08152f85de61101a4aa9699611
Hung-I Wang <whygowe+for-git@gmail.com>
2020-10-19 20:50:02 +0800

    Comment out some code from the template in main.ts

--- src/main.ts
+++ src/main.ts
@@ -45,14 +45,14 @@ async function run(): Promise<void> {
     await git.push()
     core.info(`Days committed: ${daysCommited}/${Object.keys(submissionCalendar).length}`)
 
-    const ms: string = "3000"
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // const ms: string = "3000"
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput("time", new Date().toTimeString())
+    // core.setOutput("time", new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
