58869df3774a4140f1d07c4d4dadfd486540a618
Hung-I Wang <whygowe+for-git@gmail.com>
2020-10-26 17:22:31 +0800

    Update git test accordingly

--- __tests__/git.test.ts
+++ __tests__/git.test.ts
@@ -1,9 +1,9 @@
 import { GitController } from "../src/git"
 
-test("getLastCommitDate", async () => {
+test("getLastAuthorDate", async () => {
   // TODO: create temporary repo
   const git = await GitController.createAsync(process.cwd())
   await git.init()
-  const timestamp = await git.getLastCommitDate()
+  const timestamp = await git.getLastAuthorDate()
   expect(timestamp >= new Date(0)).toBeTruthy()
 })
